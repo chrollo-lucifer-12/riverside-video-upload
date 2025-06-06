0830a2abc5f61fd84995b14d4a6db181c601ab4a
sahil-singh1107 <sahilpython@gmail.com>
2025-06-07 01:49:08 +0530

    changed to mux

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,13 @@ import multer from "multer"
 import dotenv from "dotenv"
 import {Upload} from "tus-js-client"
 dotenv.config()
+import axios from "axios"
 
+import Mux from '@mux/mux-node';
+const mux = new Mux({
+    tokenId: process.env.MUX_TOKEN_ID,
+    tokenSecret: process.env.MUX_SECRET_KEY
+});
 
 const app = express();
 app.use(cors());
@@ -12,8 +18,6 @@ app.use(express.json());
 
 const storage = multer.memoryStorage()
 const upload = multer({storage})
-const projectId = process.env.PROJECT_ID!
-const supabaseAnonKey = process.env.ANON_KEY!
 
 app.use(upload.single("file"))
 
@@ -23,33 +27,21 @@ app.post("/api/v1/upload", async (req, res) => {
 
     try {
 
-        const upload = new Upload(file?.buffer!, {
-            endpoint: `https://${projectId}.supabase.co/storage/v1/upload/resumable`,
-            headers: {
-                'x-upsert': 'true',
-                'Authorization': `Bearer ${supabaseAnonKey}`,
-                'apikey': supabaseAnonKey,
+        const uploadUrl = await mux.video.uploads.create({
+            new_asset_settings: {
+                playback_policies: ["public"],
+                video_quality: 'basic',
             },
-            uploadDataDuringCreation: true,
-            removeFingerprintOnSuccess: true,
-            metadata: {
-                bucketName : "uploads",
-                objectName: file?.originalname!,
-                contentType: file?.mimetype!,
-                cacheControl: "3600",
-            },
-            chunkSize: 6 * 1024 * 1024,
-            onProgress: (uploaded, total) => {
-                console.log(`Progress: ${((uploaded / total) * 100).toFixed(2)}%`)
-            },
+            cors_origin: "*"
         })
-        const previousUploads = await upload.findPreviousUploads()
-        if (previousUploads.length) {
-            upload.resumeFromPreviousUpload(previousUploads[0])
-        }
 
-        upload.start()
+        const uploadResponse = await axios.put(uploadUrl.url, file?.buffer, {
+            headers: {
+                'Content-Type': file?.mimetype
+            }
+        });
 
+        console.log(uploadResponse);
 
         res.status(200).json({message : "Uploaded"})
         return
